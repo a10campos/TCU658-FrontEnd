2716ec9316db9e600d5a20b9de692e9de3f14bc1
a10campos <a10.campos@hotmail.com>
2023-01-23 12:10:10 -0600

    Fix:corregido el error del flip card en moviles

--- src/Components/FlipCards/index.js
+++ src/Components/FlipCards/index.js
@@ -2,17 +2,17 @@ export default function Cards (props) {
 
     return (
         <>
-                <div className="group h-[200px] w-[200px] [perspective:1000px] m-2">
-                    <div className="relative h-full w-full rounded-xl shadow-xl  transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] pointer-events-auto">
-                        <div className="flex flex-col inset-0 object-cover h-full w-full border rounded-xl justify-center items-center">
+                <div className="group h-[180px] w-[180px] [perspective:1000px] m-2  rounded-xl border bg-[#f5f5f5] flex flex-col">
+                    <div className="relative h-full w-full rounded-xl shadow-xl  transition-all duration-500 [transform-style:preserve-3d] lg:group-hover:[transform:rotateY(180deg)] group-active:[transform:rotateY(180deg)]">
+                        <div className="flex flex-col inset-0 object-cover h-full w-full rounded-xl justify-center items-center">
                                 <img alt=" "class="w-24 h-24 rounded-full shadow-xl" src={props.url}/>
                                 <h5 class="mb-1 text-xl font-medium text-gray-900">{props.title}</h5>
                                 <span class="text-sm text-gray-500 dark:text-gray-400">{props.titleSpanish}</span>
                         </div>
                         {/*Card back */}
-                        <div className="absolute inset-0 h-full w-full rounded-xl bg-blue text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-                            <div className="flex items-center justify-center border w-full h-full">
-                                <h1> Saludo informal </h1>
+                        <div className="absolute text-black inset-0 h-full w-full rounded-xl bg-blue text-center lg:text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                            <div className="flex items-center justify-center w-full h-full">
+                                <h1> Saludo informal entre las 5pm a las 6pm de la tarde </h1>
                             </div>
                         </div>
                     </div>
