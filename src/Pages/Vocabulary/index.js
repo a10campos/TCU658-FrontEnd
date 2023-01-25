fe6c1afe13600a0d0dec62f51a9c604d013568f7
a10campos <a10.campos@hotmail.com>
2023-01-25 16:03:00 -0600

    Fix: Fixed hover in ios

--- src/Pages/Vocabulary/index.js
+++ src/Pages/Vocabulary/index.js
@@ -7,16 +7,18 @@ function Vocabulary() {
   return (
     <div className="App">
       <Header></Header>
-      <h1 className="flex items-center justify-center m-4 text-4xl font-bold">Vocabulary</h1>
-      <div className="flex flex-wrap justify-center mt-4">
-      {
-      vocabularies.map((i) => {
-        return(
-          <FlipCards title={i.title} titleSpanish={i.titleSpanish}url={i.img}/>
-        )
-      })
-      }
-      </div>
+      <body ontouchstart="">
+        <h1 className="flex items-center justify-center m-4 text-4xl font-bold">Vocabulary</h1>
+        <div className="flex flex-wrap justify-center mt-4">
+        {
+        vocabularies.map((i) => {
+          return(
+            <FlipCards title={i.title} titleSpanish={i.titleSpanish}url={i.img}/>
+          )
+        })
+        }
+        </div>
+      </body>
       <Footer></Footer>
     </div>
   );
