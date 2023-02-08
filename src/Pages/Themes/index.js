3fca57343f43e39a6a0215821ab78483cb378c1a
a10campos <a10.campos@hotmail.com>
2023-02-08 14:50:47 -0600

    Add: Added Page Themes

---
+++ src/Pages/Themes/index.js
@@ -0,0 +1,29 @@
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+import Cards from "../../Components/Cards";
+import level from "../../data/themes.json"
+import { useParams } from "react-router-dom";
+
+export default function Themes() {
+    const {level_id,unit_id} = useParams();
+    return (
+        <>
+            <div>
+                <Header></Header>
+                <div className="lg:min-h-[450px] min-h-[590px]">
+                    <h1 className=" flex items-center justify-center text-4xl font-bold mt-10 mb-8">Themes</h1>
+                    <div className=" flex flex-wrap justify-center"> 
+                        {
+                            level.map((i) => {
+                                return(
+                                <Cards title={i.Description} url={i.img} link={`${level_id}/${unit_id}/${i.id}`}/>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <Footer></Footer>
+            </div>
+        </>
+    );
+}
