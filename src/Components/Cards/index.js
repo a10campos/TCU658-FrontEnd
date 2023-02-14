44057df80f6bb6dbc10574d84d3f0dae7d3bea22
a10_campos <AARON.CAMPOSRAMIREZ@ucr.ac.cr>
2023-02-14 17:51:24 -0600

    Fix: Changes in styles of cards

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -8,12 +8,12 @@ export default function Cards (props) {
                     <div className="flex bg-[#f6f6f6] rounded-md shadow-[0_2px_4px_0px_rgba(0,0,0,0.5)] overflow-hidden min-h-[86px]">      
                         <div className="flex flex-col whitespace-nowrap overflow-hidden text-ellipsis">
                             <div className="relative rounded-[0px]">
-                                <div className="w-[200px] h-[225px] bg-white flex items-center justify-center">
+                                <div className="w-[250px] h-[300px] bg-white flex items-center justify-center">
                                     <img alt=" "className="w-[100%] h-auto object-center object-cover" src={props.url}/>
                                 </div>
 
                             </div>
-                                <div className="flex max-w-[300px] font-semibold leading-5 box-border text-ellipsis overflow-hidden py-4 text-2xl justify-center">
+                                <div className="max-w-[250px] font-semibold leading-5 box-border overflow-hidden text-ellipsis py-4 px-1  text-2xl text-center">
                                 {props.title}
                                 </div>
                                 <span class="text-sm text-gray-500 text-center pb-1 ">{props.titleSpanish}</span>
