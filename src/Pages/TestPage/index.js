2f417618e8db379d520ef1975a6fb1a3d98a2ad9
a10_campos <AARON.CAMPOSRAMIREZ@ucr.ac.cr>
2023-02-15 14:25:09 -0600

    Add:Changes in the test page

--- src/Pages/TestPage/index.js
+++ src/Pages/TestPage/index.js
@@ -2,10 +2,20 @@ import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import vocabularies from "../../data/vocabulary.json";
 import TestCards from "../../Components/TestCards";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
+//import {useState,useEffect} from "react"
 
 export default function TestPage () {
-  const {theme_id} = useParams();
+  const actualQuestion = 0;
+  //const [actualQuestion,setActualQuetion] = useState(0);
+  /*const [score,setScore] = useState(0);
+  const [isFinished,setIsFinished] = useState(false);
+
+  function handleAnswerSubmit() {
+    
+  }*/
+  
+  //const {theme_id} = useParams();
     return (
         <>
         <Header></Header>
@@ -13,20 +23,12 @@ export default function TestPage () {
         <h1 className="flex items-center justify-center m-4 text-4xl font-bold mt-10 mb-8">Practice</h1>
           <div className="w-[100%] flex flex-wrap mt-4 items-center px-2 justify-center">
           {
-          vocabularies.map((i) => {
-              if (theme_id === i.id_theme) {
-                return(
-                  <TestCards title={i.title} titleSpanish={i.titleSpanish}url={i.img}/>
-                )
-              }
-              return (
-                <></>
-              )
-          })
+             <TestCards title={vocabularies[actualQuestion].title} titleSpanish={vocabularies[actualQuestion].titleSpanish}url={vocabularies[actualQuestion].img}/>
           }
           </div>
       </body>
       <Footer></Footer>
+     
         </>
     );
 }
