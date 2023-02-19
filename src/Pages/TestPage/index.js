7b9d818053c5ceeaddddea507de6b17b8eeafdef
a10_campos <AARON.CAMPOSRAMIREZ@ucr.ac.cr>
2023-02-19 10:52:40 -0600

    Add: Added initial score to Practice page

--- src/Pages/TestPage/index.js
+++ src/Pages/TestPage/index.js
@@ -1,19 +1,23 @@
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import vocabularies from "../../data/vocabulary.json";
-import TestCards from "../../Components/TestCards";
-//import { useParams } from "react-router-dom";
-//import {useState,useEffect} from "react"
+import {useState} from "react"
 
 export default function TestPage () {
-  const actualQuestion = 0;
-  //const [actualQuestion,setActualQuetion] = useState(0);
-  /*const [score,setScore] = useState(0);
-  const [isFinished,setIsFinished] = useState(false);
+  const [respuesta,setRespuesta] = useState("");
+  const [actualQuestion,setActualQuetion] = useState(0);
+  const [score,setScore] = useState(0);
 
   function handleAnswerSubmit() {
- 
-  }*/
+    if (respuesta === vocabularies[actualQuestion].title) {
+      setScore(score + 1);
+    } 
+
+    else {
+
+    }
+    setActualQuetion(actualQuestion+1);
+  }
   
   //const {theme_id} = useParams();
     return (
@@ -21,10 +25,18 @@ export default function TestPage () {
         <Header></Header>
         <body className="h-full">
         <h1 className="flex items-center justify-center m-4 text-4xl font-bold mt-10 mb-8">Practice</h1>
-          <div className="w-[100%] flex flex-wrap mt-4 items-center px-2 justify-center">
-          {
-             <TestCards title={vocabularies[actualQuestion].title} titleSpanish={vocabularies[actualQuestion].titleSpanish}url={vocabularies[actualQuestion].img}/>
-          }
+        <span className="flex  p-2 justify-center itemns-center ">Score: {score}</span>
+          <div className="w-[100%] flex flex-col mt-4 items-center px-2 justify-center">
+            <div className="border flex flex-col h-[250px] w-[200px] rounded-xl items-center m-2">
+              <img alt=" "className=" mt-4 w-32 h-32 rounded-xl shadow-2xl" src={vocabularies[actualQuestion].img}/>
+              <input autoComplete="off"
+              onChange={(evt) => {setRespuesta(evt.target.value)}}
+              placeholder="What it's" className=" p-2 rounded-md placeholder:text-center w-[80%] text-xl border border-[#e6e6e6] mt-4"></input>
+              <span className=" text-md text-gray-500 dark:text-gray-400">{vocabularies[actualQuestion].titleSpanish}</span>
+            </div>
+            <div>
+              <button className="border rounded-xl p-2 w-[150px] bg-blue text-white" onClick={handleAnswerSubmit}> Siguiente </button>
+            </div>
           </div>
       </body>
       <Footer></Footer>
